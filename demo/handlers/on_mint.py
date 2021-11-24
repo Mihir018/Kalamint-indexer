9ec2e63b0bc3b649fd9b15f167bee5e13d8247e8
Mihir018 <mihirkasundra1@gmail.com>
2021-11-24 18:39:04 +0530

    mint entrypoint indexed

--- demo/handlers/on_mint.py
+++ demo/handlers/on_mint.py
@@ -1,11 +1,19 @@
 
+from demo.types.kalamint_1.parameter.mint import MintParameter
+from dipdup.models import Transaction
 from demo.types.kalamint_1.storage import Kalamint1Storage
 from dipdup.context import HandlerContext
-from dipdup.models import Transaction
-from demo.types.kalamint_1.parameter.mint import MintParameter
+import demo.models as models
 
 async def on_mint(
     ctx: HandlerContext,
     mint: Transaction[MintParameter, Kalamint1Storage],
 ) -> None:
-    ...
+    ctx.logger.info('Processing `%s`', mint.parameter)
+
+    token = models.Token(
+        id=mint.parameter.token_id,
+        creator=mint.parameter.creator_name,
+        collection = mint.parameter.collection_name,
+    )
+    await token.save()
--- demo/models.py
+++ demo/models.py
@@ -1,9 +1,10 @@
+from typing import Collection
 from tortoise import Model, fields
 
 
-class ExampleModel(Model):
-    id = fields.IntField(pk=True)
-    ...
+class Token(Model):
+    id = fields.BigIntField(pk=True)
+    creator = fields.CharField(max_length=100)
+    collection = fields.CharField(max_length=1000)
 
-    class Meta:
-        table = 'example_models'
+
